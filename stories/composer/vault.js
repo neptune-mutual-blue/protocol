e551012750ab65cd53ecfb7a784ca5bb278d51e1
tokyo <tokyo@neptunemutual.com>
2021-07-19 07:37:28 -0700

    Fixed test

--- stories/composer/vault.js
+++ stories/composer/vault.js
@@ -1,15 +1,21 @@
 const { storeUtil } = require('../../util')
 
+/**
+ * Gets the vault from store
+ * @param {Contracts} contracts All contracts
+ * @param {string} coverKey The cover Key
+ * @returns {any} Returns the vault contract
+ */
 const getVault = async (contracts, coverKey) => {
   const vaultAddress = await storeUtil.getVaultAddress(contracts.store, coverKey)
 
   const Vault = await ethers.getContractFactory('Vault', {
     libraries: {
-      StoreKeyUtil: contracts.storeKeyUtil.address,
-      ProtoUtilV1: contracts.protoUtilV1.address,
-      NTransferUtilV2: contracts.transferLib.address,
-      RegistryLibV1: contracts.registryLib.address,
-      ValidationLibV1: contracts.validationLib.address
+      StoreKeyUtil: contracts.libs.storeKeyUtil.address,
+      ProtoUtilV1: contracts.libs.protoUtilV1.address,
+      NTransferUtilV2: contracts.libs.transferLib.address,
+      RegistryLibV1: contracts.libs.registryLib.address,
+      ValidationLibV1: contracts.libs.validationLib.address
     }
   })
 
