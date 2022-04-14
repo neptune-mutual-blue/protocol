a1b4764cea4084518eb2c36a865f17685dff0903
flashburst <82953782+flashburst@users.noreply.github.com>
2022-04-14 12:39:36 +0530

    Added tests for `StoreKeyUtil` library

--- contracts/mock/lib-user/MockStoreKeyUtilUser.sol
+++ contracts/mock/lib-user/MockStoreKeyUtilUser.sol
@@ -11,66 +11,144 @@ contract MockStoreKeyUtilUser {
     s = store;
   }
 
+  function setUintByKey(bytes32 key, uint256 value) external {
+    s.setUintByKey(key, value);
+  }
+
+  function setUintByKeys(
+    bytes32 key1,
+    bytes32 key2,
+    uint256 value
+  ) external {
+    s.setUintByKeys(key1, key2, value);
+  }
+
+  function setUintByKeys(
+    bytes32 key1,
+    bytes32 key2,
+    address account,
+    uint256 value
+  ) external {
+    s.setUintByKeys(key1, key2, account, value);
+  }
+
+  function addUintByKey(bytes32 key, uint256 value) external {
+    s.addUintByKey(key, value);
+  }
+
+  function addUintByKeys(
+    bytes32 key1,
+    bytes32 key2,
+    uint256 value
+  ) external {
+    s.addUintByKeys(key1, key2, value);
+  }
+
+  function addUintByKeys(
+    bytes32 key1,
+    bytes32 key2,
+    address account,
+    uint256 value
+  ) external {
+    s.addUintByKeys(key1, key2, account, value);
+  }
+
+  function subtractUintByKey(bytes32 key, uint256 value) external {
+    s.subtractUintByKey(key, value);
+  }
+
+  function subtractUintByKeys(
+    bytes32 key1,
+    bytes32 key2,
+    uint256 value
+  ) external {
+    s.subtractUintByKeys(key1, key2, value);
+  }
+
+  function subtractUintByKeys(
+    bytes32 key1,
+    bytes32 key2,
+    address account,
+    uint256 value
+  ) external {
+    s.subtractUintByKeys(key1, key2, account, value);
+  }
+
   function setStringByKey(bytes32 key, string memory value) external {
     s.setStringByKey(key, value);
   }
 
-  function getStringByKey(bytes32 key) external view returns (string memory) {
-    return s.getStringByKey(key);
+  function setStringByKeys(
+    bytes32 key1,
+    bytes32 key2,
+    string memory value
+  ) external {
+    s.setStringByKeys(key1, key2, value);
+  }
+
+  function setBytes32ByKey(bytes32 key, bytes32 value) external {
+    s.setBytes32ByKey(key, value);
+  }
+
+  function setBytes32ByKeys(
+    bytes32 key1,
+    bytes32 key2,
+    bytes32 value
+  ) external {
+    s.setBytes32ByKeys(key1, key2, value);
+  }
+
+  function setBoolByKey(bytes32 key, bool value) external {
+    s.setBoolByKey(key, value);
+  }
+
+  function setBoolByKeys(
+    bytes32 key1,
+    bytes32 key2,
+    bool value
+  ) external {
+    s.setBoolByKeys(key1, key2, value);
+  }
+
+  function setBoolByKeys(
+    bytes32 key,
+    address account,
+    bool value
+  ) external {
+    s.setBoolByKeys(key, account, value);
   }
 
   function setAddressByKey(bytes32 key, address value) external {
     s.setAddressByKey(key, value);
   }
 
-  function getAddressByKey(bytes32 key) external view returns (address) {
-    return s.getAddressByKey(key);
+  function setAddressByKeys(
+    bytes32 key1,
+    bytes32 key2,
+    address value
+  ) external {
+    s.setAddressByKeys(key1, key2, value);
   }
 
-  function deleteAddressByKey(bytes32 key) external {
-    s.deleteAddressByKey(key);
+  function setAddressByKeys(
+    bytes32 key1,
+    bytes32 key2,
+    bytes32 key3,
+    address value
+  ) external {
+    s.setAddressByKeys(key1, key2, key3, value);
   }
 
-  // Used for testing count method
-  function setAddressArrayByKey(bytes32 key1, address value) external {
-    s.setAddressArrayByKey(key1, value);
+  function setAddressArrayByKey(bytes32 key, address value) external {
+    s.setAddressArrayByKey(key, value);
   }
 
-  // Single key
-  function deleteAddressArrayByKey(bytes32 key1, address value) external {
-    s.deleteAddressArrayByKey(key1, value);
-  }
-
-  // Single key
-  function countAddressArrayByKey(bytes32 key1) external view returns (uint256) {
-    return s.countAddressArrayByKey(key1);
-  }
-
-  // Used for testing count method
   function setAddressArrayByKeys(
     bytes32 key1,
     bytes32 key2,
     address value
   ) external {
     s.setAddressArrayByKeys(key1, key2, value);
-  }
-
-  function getAddressArrayByKeys(bytes32 key1, bytes32 key2) external view returns (address[] memory) {
-    return s.getAddressArrayByKeys(key1, key2);
-  }
-
-  // 2 keys
-  function deleteAddressArrayByKeys(
-    bytes32 key1,
-    bytes32 key2,
-    address value
-  ) external {
-    s.deleteAddressArrayByKeys(key1, key2, value);
-  }
-
-  // 2 keys
-  function countAddressArrayByKeys(bytes32 key1, bytes32 key2) external view returns (uint256) {
-    return s.countAddressArrayByKeys(key1, key2);
   }
 
   function setAddressArrayByKeys(
@@ -82,31 +160,21 @@ contract MockStoreKeyUtilUser {
     s.setAddressArrayByKeys(key1, key2, key3, value);
   }
 
-  function getAddressArrayByKeys(
-    bytes32 key1,
-    bytes32 key2,
-    bytes32 key3
-  ) external view returns (address[] memory) {
-    return s.getAddressArrayByKeys(key1, key2, key3);
-  }
-
-  // 3 keys
-  function deleteAddressArrayByKeys(
-    bytes32 key1,
-    bytes32 key2,
-    bytes32 key3,
-    address value
+  function setAddressBooleanByKey(
+    bytes32 key,
+    address account,
+    bool value
   ) external {
-    s.deleteAddressArrayByKeys(key1, key2, key3, value);
+    s.setAddressBooleanByKey(key, account, value);
   }
 
-  // 3 keys
-  function countAddressArrayByKeys(
+  function setAddressBooleanByKeys(
     bytes32 key1,
     bytes32 key2,
-    bytes32 key3
-  ) external view returns (uint256) {
-    return s.countAddressArrayByKeys(key1, key2, key3);
+    address account,
+    bool value
+  ) external {
+    s.setAddressBooleanByKeys(key1, key2, account, value);
   }
 
   function setAddressBooleanByKeys(
@@ -119,6 +187,164 @@ contract MockStoreKeyUtilUser {
     s.setAddressBooleanByKeys(key1, key2, key3, account, value);
   }
 
+  function deleteUintByKey(bytes32 key) external {
+    s.deleteUintByKey(key);
+  }
+
+  function deleteUintByKeys(bytes32 key1, bytes32 key2) external {
+    s.deleteUintByKeys(key1, key2);
+  }
+
+  function deleteBytes32ByKey(bytes32 key) external {
+    s.deleteBytes32ByKey(key);
+  }
+
+  function deleteBytes32ByKeys(bytes32 key1, bytes32 key2) external {
+    s.deleteBytes32ByKeys(key1, key2);
+  }
+
+  function deleteBoolByKey(bytes32 key) external {
+    s.deleteBoolByKey(key);
+  }
+
+  function deleteBoolByKeys(bytes32 key1, bytes32 key2) external {
+    s.deleteBoolByKeys(key1, key2);
+  }
+
+  function deleteBoolByKeys(bytes32 key, address account) external {
+    s.deleteBoolByKeys(key, account);
+  }
+
+  function deleteAddressByKey(bytes32 key) external {
+    s.deleteAddressByKey(key);
+  }
+
+  function deleteAddressByKeys(bytes32 key1, bytes32 key2) external {
+    s.deleteAddressByKeys(key1, key2);
+  }
+
+  function deleteAddressByKeys(
+    bytes32 key1,
+    bytes32 key2,
+    bytes32 key3
+  ) external {
+    s.deleteAddressByKeys(key1, key2, key3);
+  }
+
+  function deleteAddressArrayByKey(bytes32 key, address value) external {
+    s.deleteAddressArrayByKey(key, value);
+  }
+
+  function deleteAddressArrayByKeys(
+    bytes32 key1,
+    bytes32 key2,
+    address value
+  ) external {
+    s.deleteAddressArrayByKeys(key1, key2, value);
+  }
+
+  function deleteAddressArrayByKeys(
+    bytes32 key1,
+    bytes32 key2,
+    bytes32 key3,
+    address value
+  ) external {
+    s.deleteAddressArrayByKeys(key1, key2, key3, value);
+  }
+
+  function deleteAddressArrayByIndexByKey(bytes32 key, uint256 index) external {
+    s.deleteAddressArrayByIndexByKey(key, index);
+  }
+
+  function deleteAddressArrayByIndexByKeys(
+    bytes32 key1,
+    bytes32 key2,
+    uint256 index
+  ) external {
+    s.deleteAddressArrayByIndexByKeys(key1, key2, index);
+  }
+
+  function deleteAddressArrayByIndexByKeys(
+    bytes32 key1,
+    bytes32 key2,
+    bytes32 key3,
+    uint256 index
+  ) external {
+    s.deleteAddressArrayByIndexByKeys(key1, key2, key3, index);
+  }
+
+  function getUintByKey(bytes32 key) external view returns (uint256) {
+    return s.getUintByKey(key);
+  }
+
+  function getUintByKeys(bytes32 key1, bytes32 key2) external view returns (uint256) {
+    return s.getUintByKeys(key1, key2);
+  }
+
+  function getUintByKeys(
+    bytes32 key1,
+    bytes32 key2,
+    address account
+  ) external view returns (uint256) {
+    return s.getUintByKeys(key1, key2, account);
+  }
+
+  function getStringByKey(bytes32 key) external view returns (string memory) {
+    return s.getStringByKey(key);
+  }
+
+  function getStringByKeys(bytes32 key1, bytes32 key2) external view returns (string memory) {
+    return s.getStringByKeys(key1, key2);
+  }
+
+  function getBytes32ByKey(bytes32 key) external view returns (bytes32) {
+    return s.getBytes32ByKey(key);
+  }
+
+  function getBytes32ByKeys(bytes32 key1, bytes32 key2) external view returns (bytes32) {
+    return s.getBytes32ByKeys(key1, key2);
+  }
+
+  function getBoolByKey(bytes32 key) external view returns (bool) {
+    return s.getBoolByKey(key);
+  }
+
+  function getBoolByKeys(bytes32 key1, bytes32 key2) external view returns (bool) {
+    return s.getBoolByKeys(key1, key2);
+  }
+
+  function getBoolByKeys(bytes32 key, address account) external view returns (bool) {
+    return s.getBoolByKeys(key, account);
+  }
+
+  function getAddressByKey(bytes32 key) external view returns (address) {
+    return s.getAddressByKey(key);
+  }
+
+  function getAddressByKeys(bytes32 key1, bytes32 key2) external view returns (address) {
+    return s.getAddressByKeys(key1, key2);
+  }
+
+  function getAddressByKeys(
+    bytes32 key1,
+    bytes32 key2,
+    bytes32 key3
+  ) external view returns (address) {
+    return s.getAddressByKeys(key1, key2, key3);
+  }
+
+  function getAddressBooleanByKey(bytes32 key, address account) external view returns (bool) {
+    return s.getAddressBooleanByKey(key, account);
+  }
+
+  function getAddressBooleanByKeys(
+    bytes32 key1,
+    bytes32 key2,
+    address account
+  ) external view returns (bool) {
+    return s.getAddressBooleanByKeys(key1, key2, account);
+  }
+
   function getAddressBooleanByKeys(
     bytes32 key1,
     bytes32 key2,
@@ -128,11 +354,77 @@ contract MockStoreKeyUtilUser {
     return s.getAddressBooleanByKeys(key1, key2, key3, account);
   }
 
-  function setBytes32ByKey(bytes32 key, bytes32 value) external {
-    s.setBytes32ByKey(key, value);
+  function countAddressArrayByKey(bytes32 key) external view returns (uint256) {
+    return s.countAddressArrayByKey(key);
   }
 
-  function getBytes32ByKey(bytes32 key) external view returns (bytes32) {
-    return s.getBytes32ByKey(key);
+  function countAddressArrayByKeys(bytes32 key1, bytes32 key2) external view returns (uint256) {
+    return s.countAddressArrayByKeys(key1, key2);
+  }
+
+  function countAddressArrayByKeys(
+    bytes32 key1,
+    bytes32 key2,
+    bytes32 key3
+  ) external view returns (uint256) {
+    return s.countAddressArrayByKeys(key1, key2, key3);
+  }
+
+  function getAddressArrayByKey(bytes32 key) external view returns (address[] memory) {
+    return s.getAddressArrayByKey(key);
+  }
+
+  function getAddressArrayByKeys(bytes32 key1, bytes32 key2) external view returns (address[] memory) {
+    return s.getAddressArrayByKeys(key1, key2);
+  }
+
+  function getAddressArrayByKeys(
+    bytes32 key1,
+    bytes32 key2,
+    bytes32 key3
+  ) external view returns (address[] memory) {
+    return s.getAddressArrayByKeys(key1, key2, key3);
+  }
+
+  function getAddressArrayItemPositionByKey(bytes32 key, address addressToFind) external view returns (uint256) {
+    return s.getAddressArrayItemPositionByKey(key, addressToFind);
+  }
+
+  function getAddressArrayItemPositionByKeys(
+    bytes32 key1,
+    bytes32 key2,
+    address addressToFind
+  ) external view returns (uint256) {
+    return s.getAddressArrayItemPositionByKeys(key1, key2, addressToFind);
+  }
+
+  function getAddressArrayItemPositionByKeys(
+    bytes32 key1,
+    bytes32 key2,
+    bytes32 key3,
+    address addressToFind
+  ) external view returns (uint256) {
+    return s.getAddressArrayItemPositionByKeys(key1, key2, key3, addressToFind);
+  }
+
+  function getAddressArrayItemByIndexByKey(bytes32 key, uint256 index) external view returns (address) {
+    return s.getAddressArrayItemByIndexByKey(key, index);
+  }
+
+  function getAddressArrayItemByIndexByKeys(
+    bytes32 key1,
+    bytes32 key2,
+    uint256 index
+  ) external view returns (address) {
+    return s.getAddressArrayItemByIndexByKeys(key1, key2, index);
+  }
+
+  function getAddressArrayItemByIndexByKeys(
+    bytes32 key1,
+    bytes32 key2,
+    bytes32 key3,
+    uint256 index
+  ) external view returns (address) {
+    return s.getAddressArrayItemByIndexByKeys(key1, key2, key3, index);
   }
 }
