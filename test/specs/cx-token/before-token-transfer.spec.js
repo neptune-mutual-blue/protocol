e88c3c4a88bdb1d35fbef6351e43b5cb159e52bb
flashburst <82953782+flashburst@users.noreply.github.com>
2022-06-10 14:59:12 +0530

    Refactored cx-token tests

--- test/specs/cx-token/before-token-transfer.spec.js
+++ test/specs/cx-token/before-token-transfer.spec.js
@@ -1,10 +1,8 @@
-const { ethers } = require('hardhat')
 const BigNumber = require('bignumber.js')
+const { key, helper } = require('../../../util')
 const { deployDependencies } = require('./deps')
-const { deployer, key, helper } = require('../../../util')
-const blockHelper = require('../../../util/block')
-
-const cache = null
+const composer = require('../../../util/composer')
+const cxTokenUtil = require('../../../util/cxToken')
 const DAYS = 86400
 
 require('chai')
@@ -13,34 +11,89 @@ require('chai')
   .should()
 
 describe('cxToken: `_beforeTokenTransfer` function', () => {
-  let libraries, cxToken, store, policy
+  let cxToken, deployed, coverKey
 
-  const coverKey = key.toBytes32('test')
+  beforeEach(async () => {
+    const [owner] = await ethers.getSigners()
+    deployed = await deployDependencies()
 
-  before(async () => {
-    const blockTimestamp = await blockHelper.getTimestamp()
-    const expiryDate = blockTimestamp.add(2, 'd').unix()
+    coverKey = key.toBytes32('foo-bar')
+    const stakeWithFee = helper.ether(10_000)
+    const initialReassuranceAmount = helper.ether(1_000_000)
+    const initialLiquidity = helper.ether(4_000_000)
+    const minReportingStake = helper.ether(250)
+    const reportingPeriod = 7 * DAYS
+    const cooldownPeriod = 1 * DAYS
+    const claimPeriod = 7 * DAYS
+    const floor = helper.percentage(7)
+    const ceiling = helper.percentage(45)
+    const reassuranceRate = helper.percentage(50)
+    const leverage = '1'
 
-    libraries = await deployDependencies()
-    store = await deployer.deploy(cache, 'MockCxTokenStore')
-    cxToken = await deployer.deployWithLibraries(cache, 'cxToken', libraries.dependencies, store.address, coverKey, helper.emptyBytes32, expiryDate)
-    policy = await deployer.deploy(cache, 'MockCxTokenPolicy', cxToken.address)
+    const requiresWhitelist = false
+    const values = [stakeWithFee, initialReassuranceAmount, minReportingStake, reportingPeriod, cooldownPeriod, claimPeriod, floor, ceiling, reassuranceRate, leverage]
+
+    const info = key.toBytes32('info')
+
+    deployed.cover.updateCoverCreatorWhitelist(owner.address, true)
+
+    await deployed.npm.approve(deployed.stakingContract.address, stakeWithFee)
+    await deployed.dai.approve(deployed.reassuranceContract.address, initialReassuranceAmount)
+
+    await deployed.cover.addCover(coverKey, false, info, deployed.dai.address, requiresWhitelist, values)
+    await deployed.cover.deployVault(coverKey)
+
+    deployed.vault = await composer.vault.getVault({
+      store: deployed.store,
+      libs: {
+        accessControlLibV1: deployed.accessControlLibV1,
+        baseLibV1: deployed.baseLibV1,
+        transferLib: deployed.transferLib,
+        protoUtilV1: deployed.protoUtilV1,
+        registryLibV1: deployed.registryLibV1,
+        validationLibV1: deployed.validationLibV1
+      }
+    }, coverKey)
+
+    await deployed.dai.approve(deployed.vault.address, initialLiquidity)
+    await deployed.npm.approve(deployed.vault.address, minReportingStake)
+    await deployed.vault.addLiquidity(coverKey, initialLiquidity, minReportingStake, key.toBytes32(''))
+
+    const amountToCover = helper.ether(100_000)
+
+    await deployed.npm.approve(deployed.governance.address, helper.ether(1000))
+
+    await deployed.dai.approve(deployed.policy.address, ethers.constants.MaxUint256)
+    await deployed.policy.purchaseCover(owner.address, coverKey, helper.emptyBytes32, '1', amountToCover, key.toBytes32(''))
+    const at = (await deployed.policy.getCxToken(coverKey, helper.emptyBytes32, '1')).cxToken
+    cxToken = await cxTokenUtil.atAddress(at, {
+      accessControlLibV1: deployed.accessControlLibV1,
+      baseLibV1: deployed.baseLibV1,
+      governanceUtilV1: deployed.governanceUtilV1,
+      policyHelperV1: deployed.policyHelperV1,
+      protoUtilV1: deployed.protoUtilV1,
+      validationLibV1: deployed.validationLibV1
+    })
   })
 
   it('must fail if an expired cxToken is transferred to a non-zero address', async () => {
-    const [owner] = await ethers.getSigners()
+    const [, bob, charles] = await ethers.getSigners()
     const amount = '100'
 
-    await store.initialize()
-    await store.registerPolicyContract(policy.address)
+    // Make bob -> policy contract
+    const previous = deployed.policy.address
+    await deployed.protocol.upgradeContract(key.PROTOCOL.CNS.COVER_POLICY, deployed.policy.address, bob.address)
 
-    await policy.callMint(coverKey, helper.emptyBytes32, owner.address, amount)
+    await cxToken.connect(bob).mint(coverKey, helper.emptyBytes32, charles.address, amount)
 
-    await ethers.provider.send('evm_increaseTime', [DAYS * 3])
+    // Revert policy contract address
+    await deployed.protocol.upgradeContract(key.PROTOCOL.CNS.COVER_POLICY, bob.address, previous)
 
-    await cxToken.burn('85')
+    await ethers.provider.send('evm_increaseTime', [DAYS * 60])
 
-    await cxToken.transfer(helper.zero1, '1')
+    await cxToken.connect(charles).burn('85')
+
+    await cxToken.connect(charles).transfer(helper.zero1, '1')
       .should.be.rejectedWith('Expired cxToken')
   })
 })
--- test/specs/cx-token/ctor-vfns.spec.js
+++ test/specs/cx-token/ctor-vfns.spec.js
@@ -1,9 +1,10 @@
 const moment = require('moment')
 const BigNumber = require('bignumber.js')
-const { deployer, key, helper } = require('../../../util')
+const { key, helper } = require('../../../util')
 const { deployDependencies } = require('./deps')
-
-const cache = null
+const composer = require('../../../util/composer')
+const cxTokenUtil = require('../../../util/cxToken')
+const DAYS = 86400
 
 require('chai')
   .use(require('chai-as-promised'))
@@ -11,16 +12,69 @@ require('chai')
   .should()
 
 describe('cxToken: Constructor', () => {
-  let libraries, cxToken, store
+  let cxToken, deployed, coverKey
 
-  const coverKey = key.toBytes32('test')
-  const expiryDate = moment(new Date()).unix()
+  beforeEach(async () => {
+    const [owner] = await ethers.getSigners()
+    deployed = await deployDependencies()
 
-  before(async () => {
-    libraries = await deployDependencies()
-    store = { address: helper.randomAddress() }
+    coverKey = key.toBytes32('foo-bar')
+    const stakeWithFee = helper.ether(10_000)
+    const initialReassuranceAmount = helper.ether(1_000_000)
+    const initialLiquidity = helper.ether(4_000_000)
+    const minReportingStake = helper.ether(250)
+    const reportingPeriod = 7 * DAYS
+    const cooldownPeriod = 1 * DAYS
+    const claimPeriod = 7 * DAYS
+    const floor = helper.percentage(7)
+    const ceiling = helper.percentage(45)
+    const reassuranceRate = helper.percentage(50)
+    const leverage = '1'
 
-    cxToken = await deployer.deployWithLibraries(cache, 'cxToken', libraries.dependencies, store.address, coverKey, helper.emptyBytes32, expiryDate)
+    const requiresWhitelist = false
+    const values = [stakeWithFee, initialReassuranceAmount, minReportingStake, reportingPeriod, cooldownPeriod, claimPeriod, floor, ceiling, reassuranceRate, leverage]
+
+    const info = key.toBytes32('info')
+
+    deployed.cover.updateCoverCreatorWhitelist(owner.address, true)
+
+    await deployed.npm.approve(deployed.stakingContract.address, stakeWithFee)
+    await deployed.dai.approve(deployed.reassuranceContract.address, initialReassuranceAmount)
+
+    await deployed.cover.addCover(coverKey, false, info, deployed.dai.address, requiresWhitelist, values)
+    await deployed.cover.deployVault(coverKey)
+
+    deployed.vault = await composer.vault.getVault({
+      store: deployed.store,
+      libs: {
+        accessControlLibV1: deployed.accessControlLibV1,
+        baseLibV1: deployed.baseLibV1,
+        transferLib: deployed.transferLib,
+        protoUtilV1: deployed.protoUtilV1,
+        registryLibV1: deployed.registryLibV1,
+        validationLibV1: deployed.validationLibV1
+      }
+    }, coverKey)
+
+    await deployed.dai.approve(deployed.vault.address, initialLiquidity)
+    await deployed.npm.approve(deployed.vault.address, minReportingStake)
+    await deployed.vault.addLiquidity(coverKey, initialLiquidity, minReportingStake, key.toBytes32(''))
+
+    const amountToCover = helper.ether(100_000)
+
+    await deployed.npm.approve(deployed.governance.address, helper.ether(1000))
+
+    await deployed.dai.approve(deployed.policy.address, ethers.constants.MaxUint256)
+    await deployed.policy.purchaseCover(owner.address, coverKey, helper.emptyBytes32, '1', amountToCover, key.toBytes32(''))
+    const at = (await deployed.policy.getCxToken(coverKey, helper.emptyBytes32, '1')).cxToken
+    cxToken = await cxTokenUtil.atAddress(at, {
+      accessControlLibV1: deployed.accessControlLibV1,
+      baseLibV1: deployed.baseLibV1,
+      governanceUtilV1: deployed.governanceUtilV1,
+      policyHelperV1: deployed.policyHelperV1,
+      protoUtilV1: deployed.protoUtilV1,
+      validationLibV1: deployed.validationLibV1
+    })
   })
 
   it('must correctly construct', async () => {
@@ -28,7 +82,11 @@ describe('cxToken: Constructor', () => {
   })
 
   it('must correctly store the storage variables', async () => {
-    (await cxToken.COVER_KEY()).should.equal(coverKey)
-    ; (await cxToken.expiresOn()).toNumber().should.equal(expiryDate)
+    const block = await ethers.provider.getBlock(await ethers.provider.getBlockNumber())
+    const day = moment.unix(block.timestamp).get('date')
+    const monthEOD = moment.unix(block.timestamp).utc().add(day >= 25 ? 1 : 0, 'month').endOf('month').unix()
+
+    ;(await cxToken.COVER_KEY()).should.equal(coverKey)
+    ;(await cxToken.expiresOn()).toNumber().should.equal(monthEOD)
   })
 })
