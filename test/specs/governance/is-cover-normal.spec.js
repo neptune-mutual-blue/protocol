a2966243ddf5d32a15a74fab9927cf3ee437be6b
sansiven <sansiven@gmail.com>
2022-07-15 08:34:32 +0545

    added test for isCoverNormal function

---
+++ test/specs/governance/is-cover-normal.spec.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-unused-expressions */
+const BigNumber = require('bignumber.js')
+const { ethers } = require('hardhat')
+const uuid = require('uuid')
+const { deployer, key } = require('../../../util')
+const { deployDependencies } = require('./deps')
+const cache = null
+
+
+require('chai')
+  .use(require('chai-as-promised'))
+  .use(require('chai-bignumber')(BigNumber))
+  .should()
+
+describe("Get all products in a cover is normal", ()=>{
+
+   let deployed, coverKey
+
+  before(async () => {
+    const [bob] = await ethers.getSigners()
+    deployed = await deployDependencies()
+      coverKey = key.toBytes32('defi')
+  })
+
+  it("must return true if all products are normal", async () => {
+    const [owner] = await ethers.getSigners()
+
+    const coverStatus = await deployed.governance.connect(owner).isCoverNormal(coverKey);
+    coverStatus.should.equal(true)
+  })
+})
